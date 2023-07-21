3579baa9a08603701fe241cb0324fc2ac946b99f
jaisy <miemail@dominio.com>
2023-07-21 08:10:50 -0500

    actulizacion 4

--- src/conf_auth.js
+++ src/conf_auth.js
@@ -2,7 +2,7 @@
 
 const conf_auth = { 
     domain: 'dev-w28y255dbnb4jht3.us.auth0.com', 
-    clientId: '26mAiJSfVB8B2xaf4vzhUf2hbkns94JR',
+    clientId: '0ltuoSXmlBf5BI6B9SocE2zNmUAkujuY',
     redirectUri: window.location.origin,
  }
 
